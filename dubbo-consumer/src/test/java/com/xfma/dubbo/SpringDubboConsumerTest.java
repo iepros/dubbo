cc4a75021afc17b3bdef76caede964644dcbc36e
maxiangfeng <mxf_main@163.com>
2018-08-02 17:20:42 +0800

    使用全注解形式进行开发dubbo

--- dubbo-consumer/src/test/java/com/xfma/dubbo/SpringDubboConsumerTest.java
+++ dubbo-consumer/src/test/java/com/xfma/dubbo/SpringDubboConsumerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.xfma.service.IUserService;
 
 /**
@@ -17,7 +18,8 @@ import com.xfma.service.IUserService;
 @ContextConfiguration("classpath:spring-dubbo-consumer.xml")
 public class SpringDubboConsumerTest {
 
-	@Autowired
+//	@Autowired  //不能再使用spring的Autowired注解
+	@Reference//使用dubbo的@Reference注解，使其创建一个本地的代理对象，交给spring
 	private IUserService userService;
 	@Test
 	public void test() {
--- dubbo-provider/src/main/java/com/xfma/service/impl/UserServiceImpl.java
+++ dubbo-provider/src/main/java/com/xfma/service/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.xfma.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.xfma.bean.User;
 import com.xfma.service.IUserService;
 
+//使用dubbo的service注解，不使用spring的service注解
+@Service
 public class UserServiceImpl implements IUserService {
 
 	public User getUserById(Integer id) {
